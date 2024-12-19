0409c8b646dce0fb3429f1570c4278d32e962e08
basitdevs <arsajid090@gmail.com>
2024-12-19 23:05:45 +0500

    fix error

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="bg-white p-3 shadow-md w-full md:w-[90%] lg:w-[70%] max-w-4xl md:rounded-lg">
       <h1 className="text-2xl lg:text-4xl font-bold text-primary font-mono text-center py-2">
-        Welcome to Funkar's Quiz
+        Welcome to Funkar&apos;s Quiz
       </h1>
       <Separator />
       <div className="grid grid-cols-1 md:grid-cols-2 p-2 md:px-6 py-3 gap-4">
